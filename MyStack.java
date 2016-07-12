0c4e36a457e7d3f68e10cb9c5d0b90ba30b7fab8
Carmina Villaflores <carminav25@gmail.com>
2016-07-11 20:42:53 -0400

    Add files
    Assignment #1 - PolynomialCalculator
    Assignment #2 - MyStack

---
+++ MyStack.java
@@ -0,0 +1,79 @@
+
+/* Assignment #2 7/11/16
+ * In MyStack, you will write your own stack. 
+ * It will have its own push, pop, and peek methods just like a real stack. 
+ * You can't use Java's library stack, since the point is to implement it from scratch.
+ * 
+ * How will you design MyStack?
+ * What fields will MyStack have and will you use any data structures?
+ * Are there considerations for time and space efficiency you can take?
+ * 
+ * Whatever design you choose, please write a detailed explanation why you chose it
+ * versus other options you thought of. What are the benefits of it over other designs?
+ * This is important since it'll help you practice coding in a smart way and it'll help
+ * you articulate your ideas to interviewers. 
+ * 
+ * Try designing this with pen and paper first before you start to code.
+ * All fields and methods you add to this file must be PRIVATE and you can only use
+ * the constructor, push, pop, and peek in main. 
+ * 
+ * 
+ * For simplicity, your stack will hold ints.
+ */
+public class MyStack {
+
+	/* Add any instance variables you need here. Make sure they are PRIVATE */
+	
+	
+	/* Do you need to initialize anything in the constructor? */
+	public MyStack() {
+		/* fill this out if necessary */
+	}
+	
+	/* PUSH
+	 * Pushes int n to the top of the stack. 
+	 */
+	public void push(int n) {
+		
+		/* erase this exception and write your code here*/
+		throw new UnsupportedOperationException();
+	}
+	
+	/* POP
+	 * Removes the int at the top of the stack and returns it. 
+	 */
+	public int pop() {
+		
+		/* erase this exception and write your code here*/
+		throw new UnsupportedOperationException();
+	}
+	
+	/* PEEK
+	 * Returns the int at the top of the stack but does not remove it.
+	 */
+	public int peek() {
+		
+		/* erase this exception and write your code here*/
+		throw new UnsupportedOperationException();
+	}
+	
+	public static void main(String[] args) {
+		
+		// Add anything to the main method, but you can only use
+		// the constructor, push, pop, and peek methods from 
+		// the MyStack class here. 
+		
+		// Create a new stack
+		MyStack s = new MyStack();
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		
+		// pop 
+		
+		
+		// peek
+
+	}
+
+}
